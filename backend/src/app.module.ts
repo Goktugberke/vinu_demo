7cb55caf1cac804c09481970aad1d4914038fcbb
Göktuğ Berke Güngören <gungorengoktugberke@gmail.com>
2026-05-23 15:11:20 +0300

    USDT transfer listener with 100k threshold part completed

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { BlockchainModule } from './blockchain/blockchain.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), BlockchainModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/blockchain/blockchain.service.ts
@@ -0,0 +1,64 @@
+import {
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+  Logger,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ethers } from 'ethers';
+
+@Injectable()
+export class BlockchainService implements OnModuleInit, OnModuleDestroy {
+  private provider!: ethers.WebSocketProvider;
+  private readonly logger = new Logger(BlockchainService.name);
+  private contract!: ethers.Contract;
+  private decimals!: number;
+
+  constructor(private readonly configService: ConfigService) {}
+
+  async onModuleInit() {
+    const url = this.configService.get<string>('ETH_WSS_URL');
+    const address = this.configService.get<string>('USDT_CONTRACT_ADDRESS');
+    const abi = [
+      'event Transfer(address indexed from, address indexed to, uint256 value)',
+    ];
+    const decimalsRaw = this.configService.get<string>('USDT_DECIMALS');
+    if (!decimalsRaw) throw new Error('USDT_DECIMALS is not defined');
+    this.decimals = parseInt(decimalsRaw, 10);
+    const minRaw = this.configService.get<string>('MIN_TRANSFER_USDT');
+    if (!url || !address || !minRaw) {
+      this.logger.error(
+        'ETH_WSS_URL, USDT_CONTRACT_ADDRESS, or MIN_TRANSFER_USDT is not defined in environment variables',
+      );
+      throw new Error(
+        'One or more required environment variables are not defined',
+      );
+    }
+    const THRESHOLD = BigInt(minRaw) * 10n ** BigInt(this.decimals);
+
+    this.provider = new ethers.WebSocketProvider(url);
+
+    this.contract = new ethers.Contract(address, abi, this.provider);
+    await this.contract.on(
+      'Transfer',
+      (
+        from: string,
+        to: string,
+        value: bigint,
+        { log }: ethers.ContractEventPayload,
+      ) => {
+        if (value < THRESHOLD) {
+          return;
+          //do nothing if transfer is below threshold
+        }
+        this.logger.log(
+          `Transfer: ${from} → ${to}, ${ethers.formatUnits(value, this.decimals)} USDT, tx: ${log.transactionHash}, block: ${log.blockNumber}`,
+        );
+      },
+    );
+  }
+
+  async onModuleDestroy() {
+    await this.provider.destroy();
+  }
+}
